e86f320ea8ccbcf6e00a00cd175ecbbf66f9b1c8
Tomza78 <Tomza78@users.noreply.github.com>
2026-04-09 07:24:26 +0000

    deploy: 6b0fbb89292f16f925de81fa9712c5dcb71184b7

--- data.js
+++ data.js
@@ -1,6 +1,69 @@
 window.dashboardData = {
   "trends": [
     {
+      "id": "1sgfnl3",
+      "source": "reddit",
+      "title": "Anyone know what’s up with AbsoluteBet Ontario Launch igaming?",
+      "url": "https://www.reddit.com/r/onlinegambling/comments/1sgfnl3/anyone_know_whats_up_with_absolutebet_ontario/",
+      "author": "Gilbert6722",
+      "score": 1,
+      "selftext": "Does anyone know what’s up with this site? I’ve been patiently waiting for this site to launch as the newest igaming online casino in Ontario Canada, I heard they will have great signup bonuses. Since getting their igaming license in August 2025 I have been watching their website and they said they will be launching on April 01 2026 (see photo) but the links on their website do not work. I was just wondering if anyone else had some more insight into this casino. It’s supposed to be the first regulated tokenized casino in Canada (using Lemonchain) so I was excited for this. ",
+      "subreddit": "onlinegambling",
+      "created_utc": 1775709145,
+      "top_comments": [
+        {
+          "body": "\nThank you for posting to /r/onlinegambling! If you are new here, please remember to read the rules in the sidebar. Don't forget to subscribe and [join our Discord](https://discord.gg/dZCqv4P4DG)!\n\n*Have a gambling problem? We strive to promote healthy, responsible gambling in this subreddit. If you feel like your gambling habits are getting out of control, please read our [Problem Gambling Wiki](https://www.reddit.com/r/gambling/wiki/problem-gambling/).*\n\n*I am a bot, and this action was performed automatically. Please [contact the moderators of this subreddit](/message/compose/?to=/r/onlinegambling) if you have any questions or concerns.*",
+          "score": 1,
+          "author": "AutoModerator"
+        }
+      ],
+      "summary_he": "הפוסט דן בהמתנה להשקת AbsoluteBet באונטריו (אפריל 2026), שצפוי להיות הקזינו המוסדר הראשון בקנדה המבוסס על טכנולוגיית טוקנים (Lemonchain). כניסת מפעילים חדשניים לשוק הרגולטורי באונטריו מצביעה על המשך צמיחת הענף, מגמה רלוונטית מאוד עבור חברות כמו Bragg Gaming הפועלות כספקיות מרכזיות בשוק זה.",
+      "category_he": "שוק ההימורים",
+      "comments_summary_he": "להלן סיכום של הנקודות המרכזיות העולות מהתגובה (הנשלחת באופן אוטומטי על ידי מנהלי הקהילה):\n\n* **חשיבות הציות לכללים:** קריאה לכל המשתמשים, ובמיוחד לחדשים שבהם, לקרוא ולהכיר את חוקי הקהילה המופיעים בסרגל הצד (Sidebar) כדי לשמור על סדר בפורום.\n* **הצטרפות לקהילה המורחבת:** עידוד המשתמשים להירשם לפורום (Subscribe) ולהצטרף לשרת הדיסקורד הרשמי לצורך אינטראקציה נוספת.\n* **קידום הימורים אחראיים:** הצהרת כוונות של הקהילה לקדם דפוסי הימורים בריאים ואחראיים בקרב חבריה.\n* **סיוע בהתמכרויות:** מתן דגש על זיהוי מצבים בהם הרגלי ההימורים יוצאים משליטה, והפניה למשאבי עזרה חיצוניים (כמו דף ה-Wiki של הפורום בנושא בעיות הימורים).\n* **ניהול ותיווך אוטומטי:** שימוש בבוט (AutoModerator) לצורך מתן מידע ראשוני חיוני וסינון תכנים, מה שמעיד על ניהול מובנה של הדיונים בפורום."
+    },
+    {
+      "id": "1sgei9g",
+      "source": "reddit",
+      "title": "Golden Hearts Games online casino is such a scam….",
+      "url": "https://www.reddit.com/r/onlinegambling/comments/1sgei9g/golden_hearts_games_online_casino_is_such_a_scam/",
+      "author": "Odd_Air_3216",
+      "score": 1,
+      "selftext": "Hey everyone, sorry this is so long, I just wanted to share this here with the hopes of saving even just one person from the experiences I’ve had there. \n\nGolden Hearts slots are incredibly tight and don’t pay well at all, but today I was playing a game called 3 Hot Chili Peppers, and I’ve never seen anything like this. \n\nI played it yesterday and was doing so-so, and thought I’d give it a try today. I know I’m a dumbass for not quitting but I ended up spending around $500 and did not hit the bonus one single time after about hitting it for a nominal amount with the first $100. \n\nI know I should’ve quit, and that’s on me, but I have never ever had a game not at least pay out a little bit. The bonus happens when chili peppers are on fire, then they’ll disintegrate and you’ll get special games added for free. \n\nAfter the first $100 I spent at least $400 more and the chili peppers were on complete and total fire pretty much the entire time and not once did I get the bonus game. I was playing anywhere from $.40 to $2. And again, it’s on me that I didn’t stop but this casino is just the biggest rip off. That’s not the only time I’ve lost my ass and not gotten what is even just the bare minimum, and again like a dumbass I went back and played there again and that’s on me. But I just wanted to try and save even just one person from wasting their money there. And not only that, they don’t have an app so when you play online on your phone, it’s ridiculous. Also, their customer support, or lack there of, is atrocious.\n\nI have played thousands of slots in my lifetime and have never seen them pay out as little as they do at this place.\n\nThese screenshots show pretty much the same thing, but it shows how the chili peppers were engulfed in flames and not once gave me a bonus game.",
+      "subreddit": "onlinegambling",
+      "created_utc": 1775705705,
+      "top_comments": [
+        {
+          "body": "\nThank you for posting to /r/onlinegambling! If you are new here, please remember to read the rules in the sidebar. Don't forget to subscribe and [join our Discord](https://discord.gg/dZCqv4P4DG)!\n\n*Have a gambling problem? We strive to promote healthy, responsible gambling in this subreddit. If you feel like your gambling habits are getting out of control, please read our [Problem Gambling Wiki](https://www.reddit.com/r/gambling/wiki/problem-gambling/).*\n\n*I am a bot, and this action was performed automatically. Please [contact the moderators of this subreddit](/message/compose/?to=/r/onlinegambling) if you have any questions or concerns.*",
+          "score": 1,
+          "author": "AutoModerator"
+        }
+      ],
+      "summary_he": "הפוסט מציג ביקורת חריפה של משתמש נגד הקזינו המקוון Golden Hearts Games, בטענה למכניקות משחק מטעות (שימוש באפקטים ויזואליים המרמזים על זכייה קרובה ללא כיסוי) ושירות לקוחות לקוי. המקרה מדגיש את המגמה הגוברת של תסכול צרכנים בתחום ה-iGaming סביב שקיפות בשיעורי ההחזר לשחקן (RTP), ומצביע על החשיבות של רגולציה הדוקה ואמינות הפלטפורמות לשמירה על אמון המשתמשים בתעשייה.",
+      "category_he": "שוק ההימורים",
+      "comments_summary_he": "להלן סיכום הנקודות המרכזיות העולות מן התגובה בשרשור (המייצגת את הנחיות הקהילה והנהלים):\n\n* **חשיבות הציות לכללים:** קיימת דרישה מפורשת מכל משתתף בקהילה לקרוא ולהבין את חוקי הפורום המופיעים בסרגל הצד (Sidebar) כדי לשמור על סדר וארגון.\n* **הרחבת הקהילה לפלטפורמות נוספות:** המנהלים מעודדים את המשתמשים להצטרף לשרת הדיסקורד הרשמי של הקבוצה כדי להעמיק את האינטראקציה החברתית מעבר לרדיט.\n* **דגש על הימורים אחראיים:** הקהילה חורטת על דגלה קידום של דפוסי הימורים בריאים ואחראיים בקרב חבריה.\n* **הכרה בסיכוני התמכרות:** קיים דגש על זיהוי מצבים שבהם הרגלי ההימורים יוצאים מכלל שליטה, תוך מתן לגיטימציה לפנייה לעזרה.\n* **הנגשת משאבי תמיכה:** הקהילה מספקת גישה ישירה למקורות מידע וסיוע (כגון \"Problem Gambling Wiki\") עבור משתמשים החווים קשיים או התמכרות.\n* **ניהול קהילה אוטומטי:** שימוש במערכות אוטומטיות (בוטים) כדי להבטיח שכל משתמש חדש ייחשף למידע הקריטי בנושאי בטיחות ואחריות מיד עם הצטרפותו."
+    },
+    {
+      "id": "1sfnzh8",
+      "source": "reddit",
+      "title": "Which online casinos are actually still worth it?",
+      "url": "https://www.reddit.com/r/onlinegambling/comments/1sfnzh8/which_online_casinos_are_actually_still_worth_it/",
+      "author": "IanisQuan_101",
+      "score": 1,
+      "selftext": "I’m not really looking for a generic “top 10” list — more like honest opinions on which online casinos are still worth checking out right now. For me, fair terms, decent RTP, and fast withdrawals are the most important things. As a starting point, I looked at this page: [https://dianbonus.net/](https://dianbonus.net/) But I’d rather hear real experiences from people here. What do you guys usually look for?",
+      "subreddit": "onlinegambling",
+      "created_utc": 1775640674,
+      "top_comments": [
+        {
+          "body": "\nThank you for posting to /r/onlinegambling! If you are new here, please remember to read the rules in the sidebar. Don't forget to subscribe and [join our Discord](https://discord.gg/dZCqv4P4DG)!\n\n*Have a gambling problem? We strive to promote healthy, responsible gambling in this subreddit. If you feel like your gambling habits are getting out of control, please read our [Problem Gambling Wiki](https://www.reddit.com/r/gambling/wiki/problem-gambling/).*\n\n*I am a bot, and this action was performed automatically. Please [contact the moderators of this subreddit](/message/compose/?to=/r/onlinegambling) if you have any questions or concerns.*",
+          "score": 1,
+          "author": "AutoModerator"
+        }
+      ],
+      "summary_he": "הפוסט דן בחיפוש של שחקנים אחר פלטפורמות הימורים אמינות המציעות תנאים הוגנים, שיעורי החזר (RTP) גבוהים ומשיכות כספים מהירות. מגמה זו מדגישה את דרישת השוק לשקיפות ואיכות, נושאים קריטיים עבור ספקיות טכנולוגיה ותוכן כמו Bragg Gaming (BRAG) הפועלות בשווקים מוסדרים. הדיון משקף מעבר של צרכנים מהסתמכות על פרסום גנרי להעדפת חווית משתמש מוכחת העומדת בסטנדרטים רגולטוריים גבוהים.",
+      "category_he": "שוק ההימורים",
+      "comments_summary_he": "להלן סיכום של הנקודות המרכזיות העולות מהדיון (על פי הטקסט שפורסם):\n\n* **חשיבות הציות לנהלים:** דגש מרכזי על כך שמשתמשים חדשים חייבים לקרוא ולהבין את חוקי הקהילה לפני השתתפות פעילה בפורום.\n* **הרחבת השיח לפלטפורמות נוספות:** הזמנה של חברי הקהילה להצטרף לשרת הדיסקורד הייעודי כדי להמשיך את הדיונים בצורה חברתית ואינטראקטיבית יותר.\n* **קידום הימורים אחראיים:** מחויבות ברורה של הקהילה לעודד הרגלי הימורים בריאים ומבוקרים בקרב המשתתפים.\n* **מתן מענה להתמכרויות:** הנגשת משאבי עזרה ומידע (כמו דף ויקי ייעודי) עבור משתמשים שמרגישים שהרגלי ההימורים שלהם יוצאים משליטה.\n* **ניטור ואוטומציה:** שימוש בבוטים אוטומטיים כדי להבטיח שכללי הבטיחות וההנחיות החשובות יוצגו באופן בולט לכל משתתף כבר בתחילת הדיון."
+    },
+    {
       "id": "1sf76cc",
       "source": "reddit",
       "title": "What sites offer the best sign/deposit bonus?",
@@ -29,37 +92,39 @@ window.dashboardData = {
       ],
       "summary_he": "הפוסט בוחן את הכדאיות הכלכלית של בונוסי הרשמה והפקדה בבתי קזינו מקוונים ואת השפעת תנאי הפדיון (wagering) על רווחיות השחקן. הדיון מדגיש את מגמת ה-\"bonus hunting\" בתעשיית ה-iGaming, נושא המהווה אתגר שיווקי ותפעולי למפעילים ולספקי טכנולוגיה כמו Bragg Gaming בניהול חויית המשתמש ושולי הרווח.",
       "category_he": "שוק ההימורים",
-      "comments_summary_he": "להלן סיכום הדיון מתוך התגובות ב-Reddit בנושאי iGaming, הימורים ופיננסים:\n\n*   **השפעת גובה הזכייה על דרישות הגלגול (Playthrough):** קל יותר לעמוד בדרישות ההימור של הבונוס אם זוכים בסכום גדול בהתחלה. לעומת זאת, זכיות קטנות נוטות \"להיאכל\" על ידי דרישות אלו לפני שניתן למשוך את הכסף.\n*   **שינוי גישה כלפי בונוסים:** מומלץ להתייחס לבונוסים כאל אמצעי להארכת זמן המשחק (\"Extended Play\") שניתן בחינם, ולא כאל כסף מובטח, כדי למנוע תסכול.\n*   **מגבלות על סכומי משיכה (Payout Caps):** בתי קזינו מקוונים מטילים לעיתים קרובות מגבלות על הסכום המקסימלי שניתן למשוך מבונוסים (במיוחד מסיבובים חינם ללא הפקדה), גם אם השחקן זכה בסכום גבוה בהרבה.\n*   **פער משמעותי בין זכייה בפועל לתשלום:** הובאה דוגמה אישית של שחקן שזכה בסכום עתק (כ-298,000 רופי) מבונוס, אך גילה שהקזינו הגביל את התשלום שלו לסכום נמוך משמעותית (50,000 רופי) בהתאם לתקנון.\n*   **חשיבות האותיות הקטנות:** המקרים שתוארו מדגישים כי תנאי השימוש של האתר (Terms and Conditions) הם הקובעים את הרווח הסופי, ולא גובה הזכייה שמופיע על המסך.\n*   **חיפוש אחר מידע ואסטרטגיות:** משתתפים בדיון משתפים ומחפשים מקורות חיצוניים לקבלת טיפים ואסטרטגיות להימורים (כמו אתרי ייעוץ ייעודיים) כדי לנסות ולשפר את סיכויי ההצלחה או להבין את חוקי המשחק."
+      "comments_summary_he": "להלן סיכום הדיון מתוך התגובות ב-Reddit בנושאי iGaming, הימורים ופיננסים:\n\n*   **השפעת גובה הזכייה על דרישות הגלגול (Playthrough):** קל יותר לעמוד בדרישות ההימור של הבונוס אם זוכים בסכום גדול בהתחלה. לעומת זאת, זכיות קטנות נוטות \"להיאכל\" על ידי דרישות אלו לפני שניתן למשוך את הכסף.\n*   **שינוי גישה כלפי בונוסים:** מומלץ להתייחס לבונוסים כאל אמצעי להארכת זמן המשחק (\"Extended Play\") שניתן בחינם, ולא כאל כסף מובטח, כדי למנוע תסכול.\n*   **מגבלות על סכומי משיכה (Payout Caps):** בתי קזינו מקוונים מטילים לעיתים קרובות מגבלות על הסכום המקסימלי שניתן למשוך מבונוסים (במיוחד מסיבובים חינם ללא הפקדה), גם אם השחקן זכה בסכום גבוה בהרבה.\n*   **פער משמעותי בין זכייה בפועל לתשלום:** הובאה דוגמה אישית של שחקן שזכה בסכום עתק (כ-298,000 רופי) מבונוס, אך גילה שהקזינו הגביל את התשלום שלו לסכום נמוך משמעותית (50,000 רופי) בהתאם לתקנון.\n*   **חשיבות האותיות הקטנות:** המקרים שתוארו מדגישים כי תנאי השימוש של האתר (Terms and Conditions) הם הקובעים את הרווח הסופי, ולא גובה הזכייה שמופיע על המסך.\n*   **חיפוש אחר מידע ואסטרטגיות:** משתתפים בדיון משתפים ומחפשים מקורות חיצוניים לקבלת טיפים ואסטרטגיות להימורים (כמו אתרי ייעוץ ייעודיים) כדי לנסות ולשפר את סיכויי ההצלחה או להבין את חוקי המשחק.",
+      "date_string": "2026-04-08",
+      "saved_at": "2026-04-08T07:20:19.522+00:00"
     }
   ],
   "xIntelligence": {
     "brag_stock": {
-      "summary": "משתמשים בטוויטר דנים בעלייה של מניית BRAG בעקבות שותפויות חדשות בתחום iGaming. יש תחזיות למחיר של 10-12 דולר בשלושה חודשים הקרובים, אך חששות מפני תנודתיות בשוק. הסנטימנט הכללי חיובי עם דגש על צמיחה פוטנציאלית.",
-      "sentiment": "חיובי",
-      "breaking": "אין חדשות חשובות מהשעות האחרונות; האחרונה הייתה דיווח על שותפות עם ספק תוכנה לפני יומיים."
+      "summary": "מניית Bragg Gaming Group (BRAG) נסחרת באופן יציב עם דגש על התרחבות בשווקים חדשים. משתמשים בטוויטר מדברים על פוטנציאל צמיחה מפעילויות iGaming, אך יש חששות ממתחרים חזקים. תחזיות מחיר נעות סביב יציבות קצרת טווח עם אפשרות עלייה אם יהיו שותפויות חדשות.",
+      "sentiment": "ניטרלי",
+      "breaking": "אין חדשות חשובות מהשעות האחרונות; המידע האחרון מדבר על יציבות המניה ללא שינויים דרמטיים."
     },
     "us_regulation": {
-      "summary": "דיונים בטוויטר על חקיקה חדשה בניו יורק להרחבת רישיונות iGaming, כולל אישור הימורים מקוונים נוספים. משתמשים מביעים תמיכה ברגולציה מחמירה יותר להגנה על צרכנים, אך חששות מפני עיכובים בפעילות. ניו יורק ממשיכה להתקדם בהסדרת השוק.",
-      "sentiment": "ניטרלי",
-      "breaking": "דיווח על הצעת חוק חדשה בניו יורק להרחבת רישיונות, שפורסם הבוקר."
+      "summary": "בניו יורק, יש התקדמות בחקיקה להרחבת רגולציית iGaming, כולל רישיונות חדשים להימורים מקוונים. משתמשים בטוויטר דנים בהשפעות על חברות כמו DraftKings ו-FanDuel, עם דגש על פוטנציאל הכנסות גבוהות. חקיקה חדשה צפויה להגביר את התחרות בשוק האמריקאי.",
+      "sentiment": "חיובי",
+      "breaking": "דיווחים על אישור זמני של רישיונות חדשים במדינות אחרות כמו ניו ג'רזי, אך לא ספציפית לניו יורק בשעות האחרונות."
     },
     "brazil_market": {
-      "summary": "משתמשים בטוויטר מדברים על רגולציה חדשה בברזיל שמאשרת רישיונות להימורים מקוונים, עם דגש על אכיפה נגד אתרים בלתי חוקיים. יש התלהבות מפני פוטנציאל צמיחה בשוק, אך חששות מעיכובים בהנפקת רישיונות. השוק נחשב להזדמנות גדולה לחברות iGaming.",
+      "summary": "שוק ההימורים בברזיל ממשיך להתפתח עם רגולציות חדשות שמאשרות רישיונות לחברות בינלאומיות. משתמשים בטוויטר מדגישים אתגרים באכיפה נגד הימורים בלתי חוקיים, אך גם הזדמנויות צמיחה. רישיונות חדשים צפויים להגדיל את ההשקעות בשוק.",
       "sentiment": "חיובי",
-      "breaking": "אין חדשות חשובות מהשעות האחרונות; האחרונה הייתה אישור רישיון ראשון לפני שבוע."
+      "breaking": "אין חדשות חשובות מהשעות האחרונות; הדיונים מתמקדים בהרחבה הצפויה של השוק ללא עדכונים מיידיים."
     },
     "netherlands_ksa": {
-      "summary": "דיונים בטוויטר על חידוש רישיונות על ידי KSA, עם אכיפה נגד הפרות של BetCity וחברות אחרות. משתמשים מציינים שיפורים באבטחת משתמשים, אך מביעים חששות מעונשים כבדים. הרגולציה בהולנד נחשבת למחמירה ומפוקחת.",
-      "sentiment": "שלילי",
-      "breaking": "דיווח על קנס חדש ל-BetCity על הפרות, שפורסם לפני שעתיים."
+      "summary": "ה-KSA בהולנד מתמקדת בחידוש רישיונות להימורים, כולל BetCity שממשיכה לפעול בהצלחה. משתמשים בטוויטר מדברים על אכיפה נגד אתרי הימורים לא מורשים, עם דגש על הגנה על צרכנים. חידושים ברישיונות צפויים להגביר את התחרות בשוק המקומי.",
+      "sentiment": "ניטרלי",
+      "breaking": "דיווחים על בדיקות אכיפה של KSA נגד הפרות, אך לא ספציפית ל-BetCity בשעות האחרונות."
     },
     "igaming_industry": {
-      "summary": "משתמשים בטוויטר דנים בעסקאות כמו רכישת חברות ושיתופי פעולה בין ספקי iGaming, עם מגמות כלליות של צמיחה דיגיטלית. יש דגש על שותפויות בטכנולוגיית AI להימורים, אך חששות ממשבר כלכלי עולמי. התעשייה ממשיכה להתרחב באזורים חדשים.",
+      "summary": "בתעשיית iGaming, יש עסקאות שותפויות חדשות כמו רכישות בין חברות גדולות, עם מגמות כלליות של מעבר לטכנולוגיות מובייל. משתמשים בטוויטר מדגישים צמיחה גלובלית, אך גם סיכונים רגולטוריים. מגמות כוללות עלייה בהימורים ספורטיביים וחדשנות ב-AR/VR.",
       "sentiment": "חיובי",
-      "breaking": "אין חדשות חשובות מהשעות האחרונות; האחרונה הייתה שותפות בין שתי חברות גדולות אתמול."
+      "breaking": "אין חדשות חשובות מהשעות האחרונות; הדיונים מתמקדים במגמות ארוכות טווח."
     },
-    "overall_sentiment": "ניטרלי",
-    "top_alert": "החדשות החשובה ביותר היא הצעת החוק החדשה בניו יורק, שעשויה להשפיע על השוק הגלובלי של iGaming."
+    "overall_sentiment": "חיובי",
+    "top_alert": "אין חדשות דחופות מטוויטר מהיום; המידע הכללי מצביע על יציבות בתעשייה עם פוטנציאל צמיחה."
   },
-  "generatedAt": "2026-04-08T07:20:22.128Z"
+  "generatedAt": "2026-04-09T07:24:25.398Z"
 };
